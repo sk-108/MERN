21787a53452f52260317b27f31da35930495b0ae
sk-108 <souravkumar31cse@gmail.com>
2023-08-20 15:02:35 +0530

    js basics

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+let a = 10
+console.log(a)
+
+// alert("after updation")
+a = a + 20
+console.log(a);
+
+var arr = ['1','4',"sourav",4,];
+console.log(arr);
+
+arr.pop()
+console.log(arr)
+arr.push(345)
+console.log(arr);
+
+var d = arr.shift();
+console.log(d);
+
+console.log(arr);
+
+
+arr.unshift(2143);
+console.log(arr);
